f433af86a0e3af70f6e6ad67d53287ad19e8984d
Mohamed Safeudeen <83001831+moahmedsafeudeen@users.noreply.github.com>
2022-01-20 17:20:58 +0530

    MVC projects

---
+++ MVC/WebApplication2/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class Department
+    {
+        public int ID { get; set; }
+        public string DeptName { get; set; }
+        public string Location { get; set; }
+        public int NumberOfStaff { get; set; }
+    }
+}
